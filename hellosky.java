8055f522752dc6568b6344f819489fb86525e3cd
mohamedadilm23 <mohamedadilm2001@gmail.com>
2024-05-27 13:13:41 +0530

    Update hellosky.java

--- hellosky.java
+++ hellosky.java
@@ -5,5 +5,6 @@ public class HelloWorld {
       System.out.println("Hello, sky");
       System.out.println("Hello, Water");
       System.out.println("Hello, plants");
+          System.out.println("Hello, animals");
     }
 }
