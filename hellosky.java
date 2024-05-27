e496cdd4be38b8b3841f01b1d4359476d5e6da2f
mohamedadilm23 <mohamedadilm2001@gmail.com>
2024-05-27 12:16:19 +0530

    Create hellosky.java

---
+++ hellosky.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+      System.out.println("Hello, World!");
+      System.out.println("Hello, sky");
+      System.out.println("Hello, Water");
+      System.out.println("Hello, plants");
+    }
+}
